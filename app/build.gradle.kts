5a7b8811d3a123d5512fed2f1aa59e66a8a7ac91
ramazan.peker <>
2024-04-24 02:42:33 +0300

    project base created

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.ramazanpeker.meditationuicompose"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.ramazanpeker.meditationuicompose"
--- app/src/main/java/com/ramazanpeker/meditationuicompose/ui/theme/Type.kt
+++ app/src/main/java/com/ramazanpeker/meditationuicompose/ui/theme/Type.kt
@@ -2,33 +2,43 @@ package com.ramazanpeker.meditationuicompose.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.ramazanpeker.meditationuicompose.R
+
+
+val gothicA1 = FontFamily(
+    listOf(
+        Font(R.font.gothica1_regular, FontWeight.Normal),
+        Font(R.font.gothica1_medium, FontWeight.Medium),
+        Font(R.font.gothica1_semibold, FontWeight.SemiBold),
+        Font(R.font.gothica1_bold, FontWeight.Bold),
+        Font(R.font.gothica1_black, FontWeight.Black),
+    )
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        color = AquaBlue,
+        fontFamily = gothicA1,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 14.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    displayLarge = TextStyle(
+        color = TextWhite,
+        fontFamily = gothicA1,
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp
+    ),
+    displayMedium = TextStyle(
+        color = TextWhite,
+        fontFamily = gothicA1,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp
     )
-    */
 )
+
+
